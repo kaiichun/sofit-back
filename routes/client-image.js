9073ddca8fb3ad7f5edc194f710dbfd5e895b156
Kaii Chun <kaiichun710@gmail.com>
2024-06-11 01:34:07 +0800

    update

--- routes/client-image.js
+++ routes/client-image.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 
-// setup storage
+// Setup storage
 const storage = multer.diskStorage({
   destination: function (request, file, cb) {
     cb(null, "clientImage/");
@@ -14,13 +14,13 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-router.post("/", upload.single("clientImage"), async (request, response) => {
-  try {
-    const clientImage_url = request.file.path;
-    response.status(200).send({ clientImage_url: clientImage_url });
-  } catch (error) {
-    response.status(400).send({ message: error._message });
+router.post("/", upload.single("clientImage"), (request, response) => {
+  if (!request.file) {
+    return response.status(400).send({ message: "No file uploaded" });
   }
+
+  const clientImage_url = request.file.path;
+  response.status(200).send({ clientImage_url: clientImage_url });
 });
 
 module.exports = router;
